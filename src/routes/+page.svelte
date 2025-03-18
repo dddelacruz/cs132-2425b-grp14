4e6b27b063a88c99c03e1748b8579a52dba9e021
dddelacruz <dddelacruz7@up.edu.ph>
2025-03-18 21:30:23 +0800

    feat: added research info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,33 @@
 
 <section>
 	<h1>Research Design</h1>
-	<h2>Research Questions</h2>
+	<h2>Main Research Question:</h2>
+	<p>How does the Philippine government allocate funds for sustainability projects, and what priorities are their current priorities? </p>
+
+	<h2>Background of the Study</h2>
+	<p>SGD 11 goal is to “Make Cities and Human Settlements Inclusive, Safe, Resilient, and Sustainable”. According to the United Nations, this SDG tackles problems related to transport, housing, and sustainable development such as Disaster Risk Reduction, accessibility, and minimization of environmental impact. Key performance indicators are used to quantify the SDG and according to the SDG watch released by the Philippine Statistics Authority, the Philippines is far from reaching the desired goals for 2023. These indicators include 11.1.1p1 or Proportion of urban population who are informal settlers, 11.5.1 or Number of deaths, missing persons and directly affected persons attributed to disasters per 100,000 population 5, and 11.6.2.p1 or Percentage of highly urbanized and other major urban centers within ambient air quality guidelines value increased among others . This study uses a dataset from NEDA which contains the state current and future projects of the Philippines to investigate the different projects being implemented by the government.
+	</p>
+
+	<h2>Description of Research Problems</h2>
+	<h3>How are project funds allocated towards sustainability?</h3>
+	<ul>
+		<li>Investigate how funds are distributed between sustainability projects as opposed to other projects</li>
+	</ul>
+	
+	<h3>What kind of sustainability projects is the current government focused on?</h3>
+	<ul>
+		<li>Investigate how funds are distributed between different sustainability projects</li>
+	</ul>
+
+	<h3>Which sectors/regions does the government prioritize?</h3>
+	<ul>
+		<li>Investigate the distribution of funds according to location.</li>
+	</ul>
+
+	<h2>Hypotheses</h2>
+	<p><b>Null Hypothesis:</b> The government is not prioritizing certain kinds of sustainability projects.</p>
+	<p><b>Alternate Hypothesis:</b> The government is prioritizing certain kinds of sustainability projects. </p>
+
 </section>
 
 <section>
