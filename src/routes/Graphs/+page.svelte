821843f9a3bca692182d3e12ca35f620726d5597
dddelacruz <dddelacruz7@up.edu.ph>
2025-04-29 19:39:33 +0800

    feat: added [ie chart for r1

--- src/routes/Graphs/+page.svelte
+++ src/routes/Graphs/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import pie_legend from '$lib/assets/pie_legend.png';
+	import pie from '$lib/assets/pie.png';
 </script>
 
 <svelte:head>
@@ -9,10 +11,82 @@
 <section>
 	<h1>Exploratory Data Analysis</h1>
 </section>
+
 <hr>
 
 <section>
-	
+	<h2>Project Graphs</h2>
+
+	<h3>1. How are project funds allocated towards sustainability?</h3>
+	<p>Total amount of funds allocated: <b>PHP 9.5684 Trillion</b></p>
+
+	<div>
+		<p><b>Pie Chart for percentage of funds allocated to each sector</b></p>
+		<div class="row">
+			<div class="column">
+				<img alt="Pie Chart per Sector" src={pie} style="height:25vw"/>
+			</div>
+			<div class="column">
+				<img alt="Pie Chart Legend" src={pie_legend} style="width:15vw"/>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<p><b>Table for amount of funds allocated to each sector</b></p>
+		<table>
+			<tr>
+				  <th>Sector</th>
+				  <th>Indicative Project Cost (in Php B)</th>
+				  <th>%</th>
+			</tr>
+			<tr>
+				<td>Physical Connectivity</td>
+				<td>8343.71</td>
+				<td>87.20</td>
+			</tr>
+			<tr>
+				<td>Water Resources</td>
+				<td>775.07</td>
+				<td>8.10</td>
+			</tr>
+			<tr>
+				<td>Agriculture</td>
+				<td>213.68</td>
+				<td>2.23</td>
+			</tr>
+			<tr>
+				<td>Health</td>
+				<td>95.73</td>
+				<td>1.00</td>
+			</tr>
+			<tr>
+				<td>Digital Connectivity</td>
+				<td>78.11</td>
+				<td>0.82</td>
+			</tr>
+			<tr>
+				<td>Education</td>
+				<td>30.56</td>
+				<td>0.32</td>
+			</tr>
+			<tr>
+				<td>Other Infrastructure</td>
+				<td>21.34</td>
+				<td>0.22</td>
+			</tr>
+			<tr>
+				<td>Power and Energy</td>
+				<td>10.19</td>
+				<td>0.10</td>
+			</tr>
+			<tr>
+				<td>TOTAL</td>
+				<td>95684</td>
+				<td>100</td>
+			</tr>
+		</table>
+	</div>
 </section>
 
 <style>
@@ -26,5 +100,25 @@
 
 	h1 {
 		width: 100%;
+	}
+
+	.row {
+  		display: flex;
+	}
+
+	.column {
+		flex: 33.33%;
+		padding: 5vh;
+	}
+
+	table {
+	border-collapse: collapse;
+	width: 50vw;
+	}
+
+	td, th {
+	border: 1px solid #000000;
+	text-align: left;
+	padding: 8px;
 	}
 </style>
