1df9e430078a277de06c8669756e7c34add0abf5
dddelacruz <dddelacruz7@up.edu.ph>
2025-05-01 23:01:00 +0800

    feat: added template for about us page

---
+++ src/lib/aboutcard/AboutCard.svelte
@@ -0,0 +1,63 @@
+<script>
+    export let avatar;
+    export let name;
+    export let desc;
+    export let link;
+</script>
+
+<div class='about-card'>
+    <div class='avatar-box'>
+        <img class='avatar' src={avatar} alt='avatar'>
+    </div>
+
+    <div class='name-box'>
+        <p class="name">{name}</p>
+    </div>
+
+    <div class='desc-box'>
+        <p class="desc">{desc}</p>
+    </div>
+
+    <div class='link-box'>
+        <p class="link">{link}</p>
+    </div>
+</div>
+
+<style>
+    p{
+        margin: 0;
+    }
+
+    .name-box{
+        height: 10%;
+    }
+
+    .desc-box {
+        height: 30%;
+        padding: 5%;
+    }
+
+    .link-box {
+        height: 10%;
+    }
+
+    .about-card{
+        border: 2px solid gray;
+        border-radius: 5px;
+        height: 60vh;
+        margin: 0 10px;
+        background-color: white;
+    }
+
+    .avatar-box{
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .avatar {
+        border-radius: 50%;
+        height: 60%;
+    }
+</style>
